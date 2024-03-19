33e929e8075add9353d69b7d8415422871a73e7f
LuisMcfly <luis.payares.ruiz@gmail.com>
2024-03-19 14:40:00 -0500

    ajusteBt

--- components/buttonList/ButtonList.jsx
+++ components/buttonList/ButtonList.jsx
@@ -6,41 +6,46 @@ import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 const CategoryButton = ({ name, categories, onCategoryChange }) => {
     const [selectedCategoryText, setSelectedCategoryText] = useState(name);
     const [isHovered, setIsHovered] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    const handleMouseEnter = () => {
-        setIsHovered(true);
-    };
-
-    const handleMouseLeave = () => {
-        setIsHovered(false);
+    const handleDropdownToggle = () => {
+        setIsDropdownOpen(!isDropdownOpen);
     };
 
     const handleSelect = (categoryId, categoryText) => {
+        setSelectedCategoryText(categoryText);
         onCategoryChange(categoryId);
-        setSelectedCategoryText(categoryText)
+    };
+
+    const handleOptionClick = (itemId, itemCategory) => {
+        // Lógica para manejar la selección de la opción aquí
+        setIsDropdownOpen(false); // Cerrar el menú desplegable al hacer clic en una opción
+        handleSelect(itemId, itemCategory); // Aquí debes definir la función handleSelect
     };
 
     return (
         <ul className='dropdown-section'>
             <li
                 className='dropdown-container'
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
+                onMouseEnter={() => setIsDropdownOpen(true)}
+                onMouseLeave={() => setIsDropdownOpen(false)}
             >
-                <div className='dropdown-section-text'>
+                <div className='dropdown-section-text' onClick={handleDropdownToggle}>
                     <p>{selectedCategoryText} </p>
-                    {isHovered ? <HorizontalRuleIcon /> : <KeyboardArrowDownIcon />}
+                    {isDropdownOpen ? <HorizontalRuleIcon /> : <KeyboardArrowDownIcon />}
                 </div>
-                <ul className='dropdown'>
-                    {categories.map((item) => (
-                        <li
-                            key={item._id}
-                            onClick={() => handleSelect(item._id, item.categorie)}
-                        >
-                            {item.categorie}
-                        </li>
-                    ))}
-                </ul>
+                {isDropdownOpen && (
+                    <ul className='dropdown'>
+                        {categories.map((item) => (
+                            <li
+                                key={item._id}
+                                onClick={() => handleOptionClick(item._id, item.categorie)}
+                            >
+                                {item.categorie}
+                            </li>
+                        ))}
+                    </ul>
+                )}
             </li>
         </ul>
     );
